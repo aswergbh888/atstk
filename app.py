a1e0e6d1f62005b38cb70abec33e56b12dfa3bb4
howard <aswergbh888@gmail.com>
2022-12-25 13:08:39 +0800

    update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
 import logging
 from flask.logging import default_handler
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-# db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db.init_app(app)
 
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
@@ -18,7 +18,7 @@ app.logger.info('Starting the Flask User Management App...')
 
 @app.route('/')
 def index():
-    app.logger.info('Show log...')
+    app.logger.info(f"DATABASE_URL environment variable: {os.getenv('DATABASE_URL')}")
     return 'atstk'
 
 
